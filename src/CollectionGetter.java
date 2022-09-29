71659201c86aca395ed8b0f7dcde79ead23cc803
1Harm <ghost9029@mail.ru>
2022-09-29 15:12:21 +0600

    obs

--- src/CollectionGetter.java
+++ src/CollectionGetter.java
@@ -1,10 +1,12 @@
 import java.text.DecimalFormat;
 public class CollectionGetter implements Runnable{
     private String stock;
+    private int startTime;
     private double price;
     private priceObservable collectionShop;
-    public CollectionGetter(priceObservable collectionShop, String newStock, double newPrice){
+    public CollectionGetter(priceObservable collectionShop,int newStartTime, String newStock, double newPrice){
         this.collectionShop = collectionShop;
+        startTime=newStartTime;
         stock = newStock;
         price = newPrice;
     }
@@ -15,20 +17,19 @@ public class CollectionGetter implements Runnable{
             }
             catch(InterruptedException e)
             {}
-            double randNum = (Math.random() * (.06)) - .03;
+            double randNum = (Math.random() * (.6)) - .3;
 
-            // Formats decimals to 2 places
 
-            DecimalFormat df = new DecimalFormat("#.##");
+            DecimalFormat decimal = new DecimalFormat("#.##");
 
-            // Change the price and then convert it back into a double
+            price = price + randNum;
 
-            price = Double.valueOf(df.format((price + randNum)));
             if(stock == "Game") ((CollectionShop) collectionShop).setGamePrice(price);
             if(stock == "Book") ((CollectionShop) collectionShop).setBookPrice(price);
             if(stock == "Art") ((CollectionShop) collectionShop).setArt(price);
-            System.out.println(stock + ": " + df.format((price + randNum)) +
-                    " " + df.format(randNum));
+
+            System.out.println(stock + ": " + decimal.format((price + randNum)) +
+                    " " + decimal.format(randNum));
 
             System.out.println();
 
--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,22 @@ public class Main {
 
         CollectionObserver observer1 = new CollectionObserver(collectionShop);
 
-        collectionShop.setGamePrice(197.00);
-        collectionShop.setBookPrice(677.60);
-        collectionShop.setArt(676.40);
+        collectionShop.setGamePrice(250.00);
+        collectionShop.setBookPrice(44.60);
+        collectionShop.setArt(30.40);
 
         CollectionObserver observer2 = new CollectionObserver(collectionShop);
 
-        collectionShop.setGamePrice(197.00);
-        collectionShop.setBookPrice(677.60);
-        collectionShop.setArt(676.40);
+        collectionShop.setGamePrice(250.00);
+        collectionShop.setBookPrice(44.60);
+        collectionShop.setArt(30.40);
 
         // Delete one of the observers
 
         collectionShop.unregister(observer2);
-        Runnable getGame = new CollectionGetter(collectionShop, "Game", 212.00);
-        Runnable getBook = new CollectionGetter(collectionShop,  "Book", 52.00);
-        Runnable getArt = new CollectionGetter(collectionShop, "Art", 123.00);
+        Runnable getGame = new CollectionGetter(collectionShop, 2,"Game", 212.00);
+        Runnable getBook = new CollectionGetter(collectionShop,  2,"Book", 52.00);
+        Runnable getArt = new CollectionGetter(collectionShop, 2,"Art", 123.00);
 
 
         new Thread(getGame).start();
